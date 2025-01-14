719576c0061543c56dfb67299854879510bdcfe0
Florian_Sebulke <fsebulke@gmail.com>
2025-01-14 09:55:11 +0100

    added contact added ButtonCyan added Card

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { Contact } from "./lib/components/Contact/Contact";
+import { ButtonCyan } from "./lib/components/Elements/ButtonCyan";
+import { Card } from "./lib/components/Elements/Card";
 import { Layout } from "./lib/components/Layout/Layout";
 import { Logo } from "./lib/components/Logo/Logo";
 import { Navbar } from "./lib/components/Navbar/Navbar";
 import "./lib/styles/index.css";
-export { Layout, Logo, Navbar };
+export { ButtonCyan, Card, Contact, Layout, Logo, Navbar };
