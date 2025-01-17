8f252c09e8beb8f2e58c55e22e2e3ba7f2b54aab
Florian_Sebulke <florian.sebulke@volkswagen.de>
2025-01-17 16:09:16 +0100

    layout navbar optional default

--- src/lib/components/Layout/Layout.tsx
+++ src/lib/components/Layout/Layout.tsx
@@ -3,16 +3,20 @@ import { Navbar } from "../Navbar/Navbar";
 
 interface LayoutProps {
   children: ReactNode;
+  nav?: boolean;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
+export const Layout: React.FC<LayoutProps> = ({
+  children,
+  nav = true,
+}: LayoutProps) => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <div className="container mx-auto px-8">
-        {<Navbar />}
+        {nav && <Navbar />}
         {children}
       </div>
     </div>
